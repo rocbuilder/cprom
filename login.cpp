30db369acb42a3d2caa119f2f2069f5eb2caba64
rocbuilder <rocbuilder@gmail.com>
2015-01-20 00:44:14 +0800

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -1,38 +1,10 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+
 using namespace std;
 
-#include "searchLogin.h" //login function, asks for username and password, checks login.txt, returns "error" if no match
 #include "login.h"
-
-string login()
-{
-	string position;
-	for (int i=0; i<=(loginTries+1); i++)
-	{
-		cout<<"Please enter username: ";
-		cin>>username;
-		cout<<"Please enter password: ";
-		cin>>password;
-		position = searchLogin(username, password);
-		if (position == "error")
-		{
-			system("CLS");
-			cout<<"Incorrect Username or Password"<<endl;
-			cout<<"You have "<<loginTries - i<<" tries left"<<endl;
-			if (i==loginTries)
-			{
-				cout<<"Please try again later"<<endl;
-				system("pause");
-				return 0;
-			}
-		}
-		if (position == "cashier"||position=="stocker"||position=="manager")
-			break;
-	}
-	return position;
-}
 
 //returns staff position, cashier, stocker, manager
 string searchLogin(string username, string password)
@@ -40,7 +12,10 @@ string searchLogin(string username, string password)
 	ifstream inLogin;
 	inLogin.open("login.txt");
 	string inUsername, inPassword, inPosition;
-	
+	if(!inLogin)
+		return "noLogin.txt";
+	else
+	{
 	//search through login.txt for matching username
 	while(!inLogin.eof())
 	{
@@ -53,5 +28,36 @@ string searchLogin(string username, string password)
 			//break is to stop loop		
 		}
 	}
-	return "error";
+	return "notFound";
+	}
+}
+
+string login()
+{
+	string position;
+	for (int i=0; i<=(loginTries+1); i++)
+	{
+		cout<<"Please enter username: ";
+		cin>>username;
+		cout<<"Please enter password: ";
+		cin>>password;
+		position = searchLogin(username, password);
+		if (position == "notFound")
+		{
+			system("CLS");
+			cout<<"Incorrect Username or Password"<<endl;
+			cout<<"You have "<<loginTries - i<<" tries left"<<endl;
+			if (i==loginTries)
+			{
+				cout<<"Please try again later"<<endl;
+				system("pause");
+				return 0;
+			}
+		}
+		if (position == "noLogin.txt")
+			return 0;
+		if (position == "cashier"||position=="stocker"||position=="manager")
+			break;
+	}
+	return position;
 }
