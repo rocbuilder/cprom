4a500c3a73ee784876329c10e71f91a2102b3d84
rocbuilder <rocbuilder@gmail.com>
2015-01-20 21:32:29 +0800

    Update searchProduct.cpp

--- searchProduct.cpp
+++ searchProduct.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 Using namespace std;
 
-void searchProduct()
+void searchProduct(string search)
+{
+ifstream inProduct;
+inProduct.open("product.txt");
+string productName;
+int price;
+vector<string> location;
+int stock;
+vector<int> tag;
+vector<string> discount;
+
+
+if (!inProduct.txt)
+cout<<"product.txt not found";
+else
+{
+while(!inProduct.eof)
 {
 
 
+ 
+
+
+}
+
+}
 
 }
