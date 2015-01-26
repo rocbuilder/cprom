a5580ba4b0410cc63b053c8250ee7ce468971ca8
rocbuilder <rocbuilder@gmail.com>
2015-01-26 15:11:41 +0800

    Update cashier.h

--- headers/cashier.h
+++ headers/cashier.h
@@ -1,6 +1,14 @@
 #ifndef __CHECKOUT__
 #define __CHECKOUT__
 
+extern string productName;
+extern double price;
+extern vector<string> location;
+extern int stock;
+extern string id;
+extern vector<string> tag;
+extern vector<string> discount;
+
 void cashier();
 
 #endif
