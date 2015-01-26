19a4d2ffeb4ef4e4e0c5c81a400c27a86ca973e6
rocbuilder <rocbuilder@gmail.com>
2015-01-26 15:08:59 +0800

    Update searchProduct.cpp

--- source/searchProduct.cpp
+++ source/searchProduct.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
+using namespace std;
 
-Using namespace std;
+//productName, price, location, stock, id, tag, (discount to be decided)
+#include "searchProduct.h"
+#include "deserialise.h"
 
-void searchProduct(string search)
+int searchProduct(string search)
 {
-ifstream inProduct;
-inProduct.open("product.txt");
-string productName;
-int price;
-vector<string> location;
-int stock;
-vector<int> tag;
-vector<string> discount;
+	ifstream inProduct;
+	inProduct.open("product.txt");
+	
+	string locationSerial, tagSerial; //discountSerial;
 
-
-if (!inProduct.txt)
-cout<<"product.txt not found";
-else
-{
-while(!inProduct.eof)
-{
-
-
- 
-
-
-}
-
-}
-
+	if (!inProduct)
+		cout<<"product.txt not found"<<endl;	
+	else
+	{
+		//need function to make search variable all uppercase, and replace spacing with underscores
+		while(!inProduct.eof())
+		{
+			inProduct>>productName>>price>>locationSerial>>stock>>id>>tagSerial; //>>discountSerial; have not decided how to encode discountSerial
+			if (search == productName || search == id)
+			{
+				location = deserialise(locationSerial);
+				tag = deserialise(tagSerial);
+				//discount = deserialise(discountSerial);
+				return 0;
+			}
+		}
+		return -1;
+	}
 }
