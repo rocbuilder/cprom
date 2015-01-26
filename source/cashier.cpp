ac817d884a0a2d5f68c7717e13a57d03ea1682ca
rocbuilder <rocbuilder@gmail.com>
2015-01-26 15:08:24 +0800

    Update cashier.cpp

--- source/cashier.cpp
+++ source/cashier.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 #include "cashier.h"
 #include "quit.h"
+#include "to_string.h"
+#include "searchProduct.h" //searchProduct(string search) function, returns 0 if successful, returns -1 if not.
+
 vector<string> checkOutList;
+vector<double> checkOutPrice;
 void checkOutMenu()
 {
 	cout<<"----------Menu----------"<<endl
@@ -25,29 +29,72 @@ void checkOutDelete()
 	cout<<"which to delete?"<<endl;	
 	cin>>toDelete;
 	checkOutList.erase(checkOutList.begin()+(toDelete-1));
+	checkOutPrice.erase(checkOutPrice.begin()+(toDelete-1));
+}
+void checkOutPurchase()
+{
+	int listLength;
+	double total=0;
+	listLength = checkOutPrice.size();
+	for(int i = 0; i < listLength; i++)
+	{
+		total += checkOutPrice[i];
+	}
+	
+
 }
 
 void checkOut()
 {
 	int listLength;
+	double totalPrice;
 	string input;
 	string prevInList="0";
+	bool error = false;
 	while (input != "3")
 	{
 		system("CLS");
 		checkOutMenu();
 		listLength = checkOutList.size();
+		totalPrice = 0;
 		cout<<left<<"Check out list:";
 		for(int i = 0; i < listLength; i++)
 		{
-			cout<<endl<<i+1<<". "<<setw(20)<<checkOutList[i];
+			cout<<endl<<left<<setw(3)<<i+1<<". "<<setw(20)<<checkOutList[i];
+			cout<<fixed<<setprecision(2)<<"$"<<checkOutPrice[i];
+			totalPrice += checkOutPrice[i];
+		}
+		cout<<endl<<right<<setw(27)<<"$"<<totalPrice;
+		if (error == true)
+		{
+			cout<<"\nitem not found"<<endl;
+			error = false;
 		}
 		cout<<endl<<"input: ";
 		cin>>input;
-		if (input == "1")
-			checkOutDelete();
+		if (input == "1" || input == "2")
+		{
+			if (input == "1")
+				checkOutDelete();
+			else
+				{
+					checkOutPurchase();
+					break;
+				}
+		}
 		else
-			checkOutList.push_back(input);
+		{
+			int result = searchProduct(input);
+			if (result == 0)
+			{
+				checkOutList.push_back(productName);
+				checkOutPrice.push_back(price/100);
+			}
+			else
+				error = true;
+			
+			
+		}
 	}
 }
 
@@ -78,6 +125,6 @@ void cashier()
 	case 1:checkOut();break;
 	case 2:checkProduct();break;
 	case 3:quit();break;
-	default:cout<<"Incorrect input, please input only the numbers on the Menu"<<endl;cashier();
+	default:cout<<"Incorrect input, please input only the numbers on the Menu"<<endl;system("pause");cashier();
 	}
 }
