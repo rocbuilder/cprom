9e02ac97037d4713afb8f79873d49076b5fbc88a
rocbuilder <rocbuilder@gmail.com>
2015-01-31 18:20:32 +0800

    Create manager.h

---
+++ headers/manager.h
@@ -0,0 +1,16 @@
+#ifndef __MANAGER__
+#define __MANAGER__
+
+#include <vector>
+
+extern string productName;
+extern double price;
+extern vector<string> location;
+extern int stock;
+extern string id;
+extern vector<string> tag;
+extern vector<string> discount;
+
+void manager();
+
+#endif
