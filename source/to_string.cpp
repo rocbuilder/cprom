38e8a97a5b08ae9d785e02eb9adb378099f6c769
rocbuilder <rocbuilder@gmail.com>
2015-01-31 18:15:46 +0800

    Update to_string.cpp

--- source/to_string.cpp
+++ source/to_string.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <sstream>
+
 using namespace std;
 
 #include "to_string.h"
 
-//to_string funtion, turns int to string, might need later.
+//to_string funtion, turns double to string, might need later.
 string to_string(double number)
 {
 	stringstream convert;
